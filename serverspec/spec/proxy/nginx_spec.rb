40836fabedaf7bf1849b9420f1cd32cce438c264
shinichi fukuda <s-fukuda@iij.ad.jp>
2017-07-20 16:44:25 +0900

    modified:   serverspec/spec/proxy/nginx_spec.rb

--- serverspec/spec/proxy/nginx_spec.rb
+++ serverspec/spec/proxy/nginx_spec.rb
@@ -1,27 +1,37 @@
 require 'spec_helper'
 
-if os[:family] == 'Redhat'
-  httpd_package = 'httpd'
-elsif os[:family] == 'Debian' || os[:family] == 'Ubuntu'
-  httpd_package = 'httpd'
-end
+# if os[:family] == 'Redhat'
+#   httpd_package = 'httpd'
+# elsif os[:family] == 'Debian' || os[:family] == 'Ubuntu'
+#   httpd_package = 'httpd'
+# end
 
 # chech: httpd installed
-describe package(httpd_package) do
-  it { should be_installed }
-end
+# describe package(httpd_package) do
+#   it { should be_installed }
+# end
 
 # check: httpd up
-describe service(httpd_package) do
-  it { should be_enabled }
-end
+# describe service(httpd_package) do
+#   it { should be_enabled }
+# end
 
 # check: deamon port
-describe port(80) do
-  it { should be_listening.with('tcp') }
-end
+# describe port(80) do
+#   it { should be_listening.with('tcp') }
+# end
 
 # check: deamon port
-describe port(443) do
-  it { should be_listening.with('tcp') }
+# describe port(443) do
+#   it { should be_listening.with('tcp') }
+# end
+
+# check: certs
+# describe x509_certificate('some_cert.pem') do
+#    it { should be_valid }
+# end
+
+## test
+describe file('/etc/httpd/conf/httpd.conf') do
+  it { should be_file }
 end
