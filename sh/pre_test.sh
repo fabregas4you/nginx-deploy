9bf8b168e5ddc1d89c5cdff9369c630231ceef36
shinichi fukuda <s-fukuda@iij.ad.jp>
2017-07-26 15:33:39 +0900

    modified:   sh/pre_test.sh

--- sh/pre_test.sh
+++ sh/pre_test.sh
@@ -26,22 +26,9 @@ copy_sslfiles () {
 copy_modsecfile () {
   cp $DIRS/$MODSCONF $DEST_MODS
 }
-## nginx
-start_ngx() {
-    configtest_q || exit 1
-    echo -n "start nginx : "
-     /etc/init.d/nginx  start
-    ret=$?
-    if [ $ret -eq 0 ]; then
-        echo "[OK]"
-    else
-        echo "[NG]"
-        exit $ret
-    fi
-}
 ## run at docker
 run_remote() {
-  sudo -s;
+  sudo -s
   cp $DIRS/$CUSTCONF $DEST_CUST
   cp $DIRS/`echo $CERTS |sed 's/\// /g' |awk '{print $3}'` $DEST_CERTS
   cp $DIRS/`echo $KEYS |sed 's/\// /g' |awk '{print $3}'` $DEST_KEYS
@@ -55,7 +42,7 @@ run_remote() {
 `nc -z -w5 $SERVER $PORTS`
 STATUS=$?
 
-if [  "$STATUS" != 0 ]; then
+if [ "$STATUS" != 0 ]; then
   echo 'tcp/20022, not OPEN'
   exit 1
 else
@@ -64,5 +51,5 @@ else
   do
     scp -i $PKEY -P $PORTS $i $TARGETS:$DIRS
   done
-  ssh -i $PKEY -p $PORTS -t -t $TARGETS "declare -f; run_remote"
+  ssh -i $PKEY -p $PORTS -t -t $TARGETS "declare -f run_remote; run_remote"
 fi
